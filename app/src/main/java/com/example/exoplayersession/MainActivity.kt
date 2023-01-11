be11a819be7b639a8d5b2461b6a328e253dc4a08
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-01-11 16:21:09 -0300

    Implement Background Player screen

--- app/src/main/java/com/example/exoplayersession/MainActivity.kt
+++ app/src/main/java/com/example/exoplayersession/MainActivity.kt
@@ -7,24 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.exoplayersession.ui.screens.playerForeground.ForegroundPlayerScreen
-import com.example.exoplayersession.ui.screens.playerForeground.ForegroundPlayerViewModel
+import com.example.exoplayersession.ui.screens.playerBackground.BackgroundPlayerScreen
 import com.example.exoplayersession.ui.theme.ExoPlayerSessionTheme
-import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExoPlayerSessionTheme {
-
-                val viewModel: ForegroundPlayerViewModel = getViewModel()
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ForegroundPlayerScreen(viewModel)
+                    BackgroundPlayerScreen()
                 }
             }
         }
