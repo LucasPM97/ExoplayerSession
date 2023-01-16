469a131f40ae09983011f7734c1b5f4b5b791f71
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-01-16 12:58:28 -0300

    Move PipPlayer to new file

---
+++ app/src/main/java/com/example/exoplayersession/ui/screens/playerPip/components/PipPlayer.kt
@@ -0,0 +1,32 @@
+package com.example.exoplayersession.ui.screens.playerPip.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.media3.session.MediaController
+import com.example.exoplayersession.ui.screens.components.PlayerView
+import com.example.exoplayersession.ui.screens.playerPip.PlayerControllerVisibility
+
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+@Composable
+fun PipPlayer(
+    modifier: Modifier,
+    mediaController: MediaController?,
+    playerControlsVisibility: PlayerControllerVisibility,
+    updatePlayerControlsVisibility: (PlayerControllerVisibility) -> Unit
+) {
+
+    PlayerView(
+        player = mediaController,
+        modifier = modifier,
+        controllerAutoShow = false,
+        update = {
+            when (playerControlsVisibility) {
+                PlayerControllerVisibility.Show -> it.showController()
+                PlayerControllerVisibility.Hide -> it.hideController()
+            }
+            if (playerControlsVisibility != PlayerControllerVisibility.None) {
+                updatePlayerControlsVisibility(PlayerControllerVisibility.None)
+            }
+        },
+    )
+}
