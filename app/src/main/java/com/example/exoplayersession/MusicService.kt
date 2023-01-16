7c1830106789ceb5ea714ccaab1bf5b221433d72
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-01-16 12:38:41 -0300

    Simplify MediaController access for No-Composable classes

--- app/src/main/java/com/example/exoplayersession/MusicService.kt
+++ app/src/main/java/com/example/exoplayersession/MusicService.kt
@@ -1,8 +1,13 @@
 package com.example.exoplayersession
 
+import android.content.ComponentName
+import android.content.Context
 import androidx.media3.common.Player
+import androidx.media3.session.MediaController
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.SessionToken
+import androidx.work.await
 import org.koin.android.ext.android.inject
 
 class MusicService : MediaSessionService() {
@@ -21,4 +26,16 @@ class MusicService : MediaSessionService() {
         }
         super.onDestroy()
     }
+}
+
+suspend fun Context.getMediaSessionController(): MediaController {
+    val sessionToken =
+        SessionToken(this, ComponentName(this, MusicService::class.java))
+
+    val controllerFuture = MediaController.Builder(
+        this,
+        sessionToken
+    ).buildAsync()
+
+    return controllerFuture.await()
 }
--- app/src/main/java/com/example/exoplayersession/ui/screens/components/RememberMediaControllerState.kt
+++ app/src/main/java/com/example/exoplayersession/ui/screens/components/RememberMediaControllerState.kt
@@ -1,39 +1,21 @@
 package com.example.exoplayersession.ui.screens.components
 
-import android.content.ComponentName
-import android.content.Context
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.media3.session.MediaController
-import androidx.media3.session.SessionToken
-import androidx.work.await
-import com.example.exoplayersession.MusicService
+import com.example.exoplayersession.getMediaSessionController
 
 @Composable
-fun rememberMediaControllerState(): MutableState<MediaController?> {
+fun rememberMediaController(): MutableState<MediaController?> {
     val context = LocalContext.current
     val mediaControllerState = remember {
         mutableStateOf<MediaController?>(null)
     }
 
     LaunchedEffect(true) {
-        mediaControllerState.value = getMediaController(context)
+        mediaControllerState.value = context.getMediaSessionController()
     }
 
     return mediaControllerState
-}
-
-private suspend fun getMediaController(context: Context): MediaController? {
-    return context.run {
-        val sessionToken =
-            SessionToken(this, ComponentName(this, MusicService::class.java))
-
-        val controllerFuture = MediaController.Builder(
-            this,
-            sessionToken
-        ).buildAsync()
-
-        controllerFuture.await()
-    }
 }
 
